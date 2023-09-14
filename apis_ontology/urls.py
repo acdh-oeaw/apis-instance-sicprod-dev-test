1879ccc0f05d065c694034cfdae154327d54570a
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-09-14 13:13:08 +0200

    feat: add django-versions dependency

--- apis_ontology/urls.py
+++ apis_ontology/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from .views import CustomReferenceDetailView, TempTripleAutocomplete, TempEntityClassAutocomplete, CustomReferenceDeleteView
 from django.contrib.auth.decorators import login_required
 
+from django_versions.urls import urlpatterns as django_version_urlpatterns
+urlpatterns = django_version_urlpatterns + urlpatterns
+
 customurlpatterns = [
     path("bibsonomy/references/<int:pk>", login_required(CustomReferenceDetailView.as_view()), name='referencedetail'),
     path('bibsonomy/tempentityclass-autocomplete/', TempEntityClassAutocomplete.as_view(), name="tempentityclass-autocomplete",),
